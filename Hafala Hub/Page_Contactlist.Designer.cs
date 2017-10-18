763b8d113f34bae6575bd14f2bc3246132cb367d
idanherman <32801307+idanherman@users.noreply.github.com>
2017-10-18 23:39:53 +0300

    Add files via upload

---
+++ Hafala Hub/Page_Contactlist.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Hafala_Hub
+{
+    partial class Page_Contactlist
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(226, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(295, 65);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Contacts List";
+            // 
+            // Page_Contactlist
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label1);
+            this.Name = "Page_Contactlist";
+            this.Size = new System.Drawing.Size(782, 583);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
